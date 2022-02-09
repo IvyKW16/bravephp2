377c28783ef29c1ae0d8be8d95fbbf41f98f05c6
IvyKW16 <97436304+IvyKW16@users.noreply.github.com>
2022-02-09 14:36:13 +0300

    PHP codes day 6
    to update information on a database

--- PHP_FILES/update.php
+++ PHP_FILES/update.php
@@ -2,22 +2,49 @@
 include "connect.php";
 
 if (isset($_POST["submit"]) and !empty ($_POST["id"])){
+    #updating here
     $id = $_POST ["id"];
+    $up_firstName= $_POST ["firstName"];
+    $up_secondName= $_POST ["secondName"];
 
-    $sql = "UPDATE `detail` SET `firstName`='$firstName',`secondName`='$secondName' WHERE id=$id";
+    $up_sql = "UPDATE `detail` SET `firstName`='$up_firstName',`secondName`='$up_secondName' WHERE id=$id";
+    $up_results = mysqli_query($link,$up_sql);
 
-    $results=mysqli_query($link, $sql);
-
-    if ($results){
-        echo "you updated the record";
-        header ("location:select.php");
-    }
-    else{
-        echo "Error executing your query $sql".mysqli_error($link);
-    }
+    if ($up_results) {
+        echo "Record has been updated";
+        header("location:select.php");
+    } else {
+        echo "Error updating record $up_sql".mysqli_error($link);
+      }
 }
 else{
-    echo "Try updating a record";
+    if (isset($_GET["id"]) and !empty ($_GET["id"])) {
+        $id= $_GET ["id"];
+
+        $sql = "SELECT * FROM `detail` WHERE id=$id";
+
+        $results=mysqli_query($link, $sql);
+
+        if ($results){
+            $data = mysqli_num_rows($results);
+
+            if ($data == 1) {
+                $row = mysqli_fetch_array($results);
+                $firstName = $row ["firstName"];
+                $secondName = $row ["secondName"];
+
+                echo "You updated the record";
+                #header("location:select.php");
+            } else{
+                echo "Error executing your query $sql".mysqli_error($link);
+            }
+        } else{
+            echo "No data found in Database";
+         }
+
+        } else{
+
+    }
 }
 
 ?>
@@ -33,20 +60,28 @@ else{
 </head>
 <body>
 
-<div>
+<h2>UPDATE THIS RECORD</h2>
+
     <form action="update.php" method="post">
 
         <div>
-            <label>Are you sure you want to update this record?</label>
-            <input type="text" name="id" value="<?php echo $_GET['id']?>">
-        </div>
-        <div>
-            <input type="submit" value="YES" name="submit">
-            <a href="select.php">NO</a>
+            <label>First Name</label>
+            <input type="text" name="firstName" value="<?php echo$firstName;?>">
         </div>
 
+        <div>
+            <label>Second Name</label>
+            <input type="text" name="secondName" value="<?php echo$secondName;?>">
+        </div>
+
+        <div>
+            <label>ID</label>
+            <input type="text" name="id" value="<?php echo $_GET["id"];?>">
+        </div>
+
+        <input type="submit" name="submit" value="Update">
+
     </form>
-</div>
 
 </body>
 </html>
